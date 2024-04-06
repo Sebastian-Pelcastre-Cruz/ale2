f98da08f69a63478e260b2937baeddf50f5442d0
Sebastian Pelcastre Cruz <sebastian.pelcastre@grupobimbo.com>
2024-04-06 14:16:32 -0600

    first commit

---
+++ home/js/constants.js
@@ -0,0 +1,21 @@
+const tarjetas = [
+    {
+      titulo: "Hola mi Amor :3",
+      descripcion:
+        "Espero que te guste mucho tu regalo que hice con mucho amor y también espero que no lo hayas sospechado jajaja",
+      imagen: "",
+    },
+    {
+      titulo: "Con mucho amor",
+      descripcion: "ajksyhdgqwudb",
+      imagen: "",
+    },
+    {
+      titulo: "Ti amuu",
+      descripcion: "oiqwueequpweouqeqcvjnbsdufhernfjcnvldj",
+      imagen: "",
+    },
+  ];
+  
+  export default tarjetas;
+  
